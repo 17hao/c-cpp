2d2bb057f720e279589beb7b048f6432547c19a6
17hao <812411866@qq.com>
2019-03-27 17:41:42 +0800

    first push

---
+++ src/code/data/show-bytes.c
@@ -0,0 +1,35 @@
+/*
+ * show-bytes.c
+ *
+ *  Created on: Mar 27, 2019
+ *      Author: 17hao
+ */
+
+#include<stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+	for (int i = 0; i < len; i++) {
+		printf("%.2x", start[i]);
+		printf("\n");
+	}
+}
+
+void show_int(int x) {
+	show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+	show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_bytes_test(int val) {
+	int ival = val;
+	float fval = (float) val;
+	show_int(ival);
+	show_float(fval);
+	printf("\n");
+	char *c = "ABCDEF";
+	show_bytes(c, strlen(c));
+}
