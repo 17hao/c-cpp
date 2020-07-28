f147cd315cdb3d8ed78fb0c52b19534ae1be96ca
17hao <sqh1107@gmail.com>
2020-07-28 23:49:04 +0800

    test directory

---
+++ test/f.s
@@ -0,0 +1,23 @@
+	.file	"f.c"
+	.text
+	.globl	f
+	.type	f, @function
+f:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	imull	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	f, .-f
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-11)"
+	.section	.note.GNU-stack,"",@progbits
---
+++ test/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+extern f(int n);
+
+int main(int argc, char **argv) {
+    int res = f(1);
+    printf("%d\n", res);	
+}
