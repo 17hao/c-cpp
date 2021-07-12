f63a503a2a53e9e8b8210259954aa8bbc20f3bb2
17hao <sqh1107@gmail.com>
2021-07-12 17:31:27 +0800

    leetcode 206

---
+++ alg/00/0206-ReverseList.c
@@ -0,0 +1,41 @@
+/*
+ * reverse linked list
+ *
+ * @since 2021-7-12 Monday 16:53 - 17:31
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "ListNode.h"
+
+
+struct ListNode* reverseList(struct ListNode* head){
+	struct ListNode* dummy = malloc(sizeof(struct ListNode));
+	dummy->next = NULL;
+    struct ListNode* curNode = head;
+	while (curNode != NULL) {
+		struct ListNode* nextNode = curNode->next;
+		curNode->next = dummy->next;
+		dummy->next = curNode;
+		curNode = nextNode;
+	}
+	return dummy->next;
+}
+
+int main(int argc, char* argv[]) {
+	struct ListNode* dummy = malloc(sizeof(struct ListNode));
+	struct ListNode* cur = dummy;
+	int n = 10;
+	for (int i = 0; i < n; i++) {
+		struct ListNode* node = malloc(sizeof(struct ListNode));
+		node->val = i;
+		cur->next = node;
+		cur = cur->next;
+	}
+	struct ListNode* res = reverseList(dummy->next);
+	cur = res;
+	while (cur != NULL) {
+		printf("%d ", cur->val);
+		cur = cur->next;
+	}
+}
