6dbdab8ec2a5ecf0a09bcb3ef70d6b9d73147ef7
17hao <sqh1107@gmail.com>
2021-09-10 16:30:28 +0800

    leetcode 86

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## c program
+将conf目录下的配置文件放入.vscode目录，即可用Run Code插件运行C/C++代码。
+
+launch.json中configurations->preLaunchTask名称为tasks.json中每个task的label
+
+优先用g++，c++版本为c++11，用clang时debug有问题。
---
+++ alg/00/0086-PartitionList.cpp
@@ -0,0 +1,36 @@
+#include "ListNodeCpp.h"
+
+/**
+ * @since 2021-9-10 Friday 15:40
+ */
+class Solution {
+   public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* dummy = new ListNode();
+        ListNode* beforeHead = new ListNode();
+        ListNode* head0 = beforeHead;
+        ListNode* afterHead = new ListNode();
+        ListNode* head1 = afterHead;
+        while (head != nullptr) {
+            if (head->val < x) {
+                head0->next = head;
+                head0 = head0->next;
+            } else {
+                head1->next = head;
+                head1 = head1->next;
+            }
+            head = head->next;
+        }
+        head1->next = nullptr;
+        head0->next = afterHead->next;
+        return beforeHead->next;
+    }
+};
+
+int main(int argc, char const* argv[]) {
+    Solution s;
+    int arr[6] = {1, 4, 3, 2, 5, 2};
+    ListNode* head = toList(arr, 6);
+    ListNode* res = s.partition(head, 3);
+    printList(res);
+}
