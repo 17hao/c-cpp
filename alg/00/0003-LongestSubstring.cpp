b925773da00d8dd404140632b87e331f2945f566
17hao <sqh1107@gmail.com>
2021-09-23 17:50:58 +0800

    improve leetcode 3

--- alg/00/0003-LongestSubstring.cpp
+++ alg/00/0003-LongestSubstring.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <set>
 #include <string>
 
@@ -27,16 +28,41 @@ class Solution {
         }
         return res;
     }
+
+    int improvedLengthOfLongestSubstring(std::string s) {
+        int start = 0, end = 0;
+        int len = s.size();
+        int res = 0;
+        std::map<char, int> map;
+        while (end < len) {
+            char c = s[end];
+            if (map.find(c) != map.end()) {
+                start = std::max(start, map[c] + 1);
+            }
+            map[c] = end;
+            res = std::max(res, (end - start) + 1);
+            end++;
+        }
+        return res;
+    }
 };
 
 int main(int argc, char const *argv[]) {
     Solution s;
+
     std::string s0 = "pwwkew";
-    std::cout << s.lengthOfLongestSubstring(s0) << "\n";
+    // std::cout << s.lengthOfLongestSubstring(s0) << "\n";
+    // std::cout << s.improvedLengthOfLongestSubstring(s0) << "\n";
 
-    std::string s1 = "aaa";
-    std::cout << s.lengthOfLongestSubstring(s1) << "\n";
+    // std::string s1 = "aaa";
+    // std::cout << s.lengthOfLongestSubstring(s1) << "\n";
+    // std::cout << s.improvedLengthOfLongestSubstring(s1) << "\n";
 
-    std::string s2 = "abcabcbb";
-    std::cout << s.lengthOfLongestSubstring(s2) << "\n";
+    // std::string s2 = "abcabcbb";
+    // std::cout << s.lengthOfLongestSubstring(s2) << "\n";
+    // std::cout << s.improvedLengthOfLongestSubstring(s2) << "\n";
+
+    std::string s3 = "abba";
+    std::cout << s.lengthOfLongestSubstring(s3) << "\n";
+    std::cout << s.improvedLengthOfLongestSubstring(s3) << "\n";
 }
