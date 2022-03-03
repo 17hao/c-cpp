350a688de4b181615cfb7d455e7a547433eb418a
shiqihao <shiqihao@bytedance.com>
2022-03-03 17:49:10 +0800

    update

--- csapp/ch07-link/sum.c
+++ csapp/ch07-link/sum.c
@@ -1,15 +1,7 @@
-# 1 "main.c"
-# 1 "<built-in>" 1
-# 1 "<built-in>" 3
-# 367 "<built-in>" 3
-# 1 "<command line>" 1
-# 1 "<built-in>" 2
-# 1 "main.c" 2
-int sum(int* a, int n);
-
-int arr[2] = {1, 2};
-
-int main() {
-    int val = sum(arr, 2);
-    return val;
+int sum(int* a, int n) {
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        s += a[i];
+    }
+    return s;
 }
